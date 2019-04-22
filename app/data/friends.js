f682872209d9dd005bc847d97fc1d246ef30ac13
Zach Drake <zadrake@yahoo.com>
2019-04-21 18:57:49 -0700

    modal working

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var userInformation = [
     {
         "name": "Jim",
-        "Photo": "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/tomclancy-jimryan-mashup.jpg",
+        "photo": "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/tomclancy-jimryan-mashup.jpg",
         "scores": [
             "5",
             "1",
@@ -18,7 +18,7 @@ var userInformation = [
     },
     {
         "name": "Pam",
-        "Photo": "https://img.buzzfeed.com/buzzfeed-static/static/2016-07/21/10/campaign_images/buzzfeed-prod-fastlane02/17-indignities-pregnant-pam-suffered-on-the-office-2-31901-1469112288-2_dblbig.jpg",
+        "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2016-07/21/10/campaign_images/buzzfeed-prod-fastlane02/17-indignities-pregnant-pam-suffered-on-the-office-2-31901-1469112288-2_dblbig.jpg",
         "scores": [
             "2",
             "4",
@@ -34,7 +34,7 @@ var userInformation = [
     },
     {
         "name": "Dwight",
-        "Photo": "https://curiousgeorge2015.files.wordpress.com/2016/02/rainn-wilson-dwight-schrute-the-office2.jpg?w=680",
+        "photo": "https://curiousgeorge2015.files.wordpress.com/2016/02/rainn-wilson-dwight-schrute-the-office2.jpg?w=680",
         "scores": [
             "1",
             "2",
@@ -50,7 +50,7 @@ var userInformation = [
     },
     {
         "name": "Angela",
-        "Photo": "https://media.wkyc.com/assets/WKYC/images/582428272/582428272_750x422.jpg",
+        "photo": "https://media.wkyc.com/assets/WKYC/images/582428272/582428272_750x422.jpg",
         "scores": [
             "2",
             "1",
@@ -66,7 +66,7 @@ var userInformation = [
     },
     {
         "name": "Kevin",
-        "Photo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Office-1200-baumgartner1.jpg/260px-Office-1200-baumgartner1.jpg",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Office-1200-baumgartner1.jpg/260px-Office-1200-baumgartner1.jpg",
         "scores": [
             "1",
             "1",
@@ -82,7 +82,7 @@ var userInformation = [
     },
     {
         "name": "Michael",
-        "Photo": "https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
         "scores": [
             "5",
             "5",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,9 +16,12 @@ module.exports = (app) => {
         var totalDifference = 1000;
 
         for (var i = 0; i < userInformation.length; i++){
+            // console.log(userInformation[i].name);
             var diff = 0;
-            for (var j = 0; userResponse.length; j++){
+            for (var j = 0; j < userResponse.length; j++){
+                // console.log(userInformation[i].scores[j]);
                 diff += Math.abs(userInformation[i].scores[j] - userResponse[j]);
+                // console.log(diff);
             }
 
             if (diff < totalDifference) {
@@ -27,9 +30,10 @@ module.exports = (app) => {
                 matchImage = userInformation[i].photo;
             }
         }
+
+        // console.log(matchName);
+        // console.log(matchImage);
         
-
-
         userInformation.push(userInput);
         
         res.json({status: 200, matchName: matchName, matchImage: matchImage});
